4b0e8f0965505fa9ef846b88234721ec9f934d81
Lihini Senanayake <lihini.sena@gmail.com>
2021-10-16 22:55:34 +0530

    fix: removed module dependencies on angular commons

--- projects/ngx-markdown-editor/src/lib/component/markdown-editor.module.ts
+++ projects/ngx-markdown-editor/src/lib/component/markdown-editor.module.ts
@@ -1,13 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+// import { CommonModule } from '@angular/common';
+// import { HttpClientModule } from '@angular/common/http';
 import { ModuleWithProviders, NgModule, SecurityContext } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MarkdownModule, MarkdownModuleConfig, SECURITY_CONTEXT } from 'ngx-markdown';
 import { MarkdownEditorComponent } from './markdown-editor.component';
 
@@ -18,10 +18,10 @@ export interface MarkdownEditorModuleConfig {
 @NgModule({
   declarations: [MarkdownEditorComponent],
   imports: [
-    CommonModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
+    // CommonModule,
+    // BrowserModule,
+    // BrowserAnimationsModule,
+    // HttpClientModule,
     MatIconModule,
     MatButtonModule,
     MatFormFieldModule,
